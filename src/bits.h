fc1bbf3a4bf59868d5a60fa395cf5f00f49b4fc3
Leo Yuriev <leo@yuriev.ru>
2017-10-29 03:23:39 +0300

    mdbx: disable 'unreachable code' for MSVC.
    Change-Id: I4f9887b557885cb9a3829d19a56d332c76656cf8

--- src/bits.h
+++ src/bits.h
@@ -48,6 +48,7 @@
 #pragma warning(disable : 4710) /* 'xyz': function not inlined */
 #pragma warning(disable : 4711) /* function 'xyz' selected for automatic inline expansion */
 #pragma warning(disable : 4201) /* nonstandard extension used : nameless struct / union */
+#pragma warning(disable : 4702) /* unreachable code */
 #pragma warning(disable : 4706) /* assignment within conditional expression */
 #pragma warning(disable : 4127) /* conditional expression is constant */
 #pragma warning(disable : 4324) /* 'xyz': structure was padded due to alignment specifier */
