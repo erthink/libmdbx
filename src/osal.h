5a1f388db5e8b12b17bc4bf4ac31e3ddcf2598af
Leo Yuriev <leo@yuriev.ru>
2018-08-01 18:16:40 +0300

    mdbx: drop unused mdbx_lck_upgrade().

--- src/osal.h
+++ src/osal.h
@@ -552,7 +552,6 @@ int mdbx_lck_init(MDBX_env *env);
 
 int mdbx_lck_seize(MDBX_env *env);
 int mdbx_lck_downgrade(MDBX_env *env, bool complete);
-int mdbx_lck_upgrade(MDBX_env *env);
 void mdbx_lck_destroy(MDBX_env *env);
 
 int mdbx_rdt_lock(MDBX_env *env);
