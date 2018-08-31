3cf6bc0639c7c4a751cf17b5f30a6f87577656e4
Leonid Yuriev <leo@yuriev.ru>
2018-08-31 19:27:32 +0300

    mdbx-test: add +/- data-dups passes into `gc.sh` script.
    Change-Id: If5c00da0bb893dccddf2d145bcd6548089984e8d

--- test/gc.sh
+++ test/gc.sh
@@ -21,14 +21,21 @@ function bits2list {
 for nops in {7..1}; do
 	for ((wbatch=nops; wbatch > 0; --wbatch)); do
 		for ((bits=2**${#options[@]}; --bits >= 0; )); do
+			seed=$(date +%N)
 			echo "=================================== $(date)"
 			rm -f ${TESTDB_PREFIX}*
-			seed=$(date +%N)
-			echo "./mdbx_test --pathname=${TESTDB_PREFIX} --pagesize=min --size=8G --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max --nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) --keygen.seed=${seed} --hill"
-			./mdbx_test --pathname=${TESTDB_PREFIX} --pagesize=min --size=8G --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max \
+			echo "./mdbx_test --pathname=${TESTDB_PREFIX} --pagesize=min --size=8G --table=-data.dups --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max --nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) --keygen.seed=${seed} --hill"
+			./mdbx_test --pathname=${TESTDB_PREFIX} --pagesize=min --size=8G --table=-data.dups --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max \
 				--nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) \
-				--keygen.seed=$(date +%N) --hill | bzip2 -c > ${TESTDB_PREFIX}.log.bz2
-			./mdbx_chk -nvv ${TESTDB_PREFIX} | tee ${TESTDB_PREFIX}-chk.log
+				--keygen.seed=${seed} --hill | lz4 > ${TESTDB_PREFIX}.log.lz4
+			./mdbx_chk -nvvv ${TESTDB_PREFIX} | tee ${TESTDB_PREFIX}-chk.log
+			echo "=================================== $(date)"
+			rm -f ${TESTDB_PREFIX}*
+			echo "./mdbx_test --pathname=${TESTDB_PREFIX} --pagesize=min --size=8G --table=+data.dups --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max --nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) --keygen.seed=${seed} --hill"
+			./mdbx_test --pathname=${TESTDB_PREFIX} --pagesize=min --size=8G --table=+data.dups --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max \
+				--nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) \
+				--keygen.seed=${seed} --hill | lz4 > ${TESTDB_PREFIX}.log.lz4
+			./mdbx_chk -nvvv ${TESTDB_PREFIX} | tee ${TESTDB_PREFIX}-chk.log
 		done
 	done
 done
