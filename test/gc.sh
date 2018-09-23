5a29214ad9f230cc7653d0707a477cf1f30024bf
Leonid Yuriev <leo@yuriev.ru>
2018-09-23 12:37:33 +0300

    mdbx-test: backport - update 'gc.sh' script (squashed).

--- test/gc.sh
+++ test/gc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-TESTDB_PREFIX=${1:-/dev/shm/mdbx-gc-test}
+make check
+TESTDB_PREFIX=${1:-/dev/shm/mdbx-gc-test}.
 
 function rep9 { printf "%*s" $1 '' | tr ' ' '9'; }
 function join { local IFS="$1"; shift; echo "$*"; }
@@ -18,16 +19,45 @@ function bits2list {
 	join , "${list[@]}"
 }
 
-for nops in {1..7}; do
-	for ((wbatch=nops; wbatch > 0; --wbatch)); do
-		for ((bits=2**${#options[@]}; --bits >= 0; )); do
-			echo "=================================== $(date)"
-			rm -f ${TESTDB_PREFIX}*
-			echo --nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits)
-			./mdbx_test --pathname=${TESTDB_PREFIX} --size=8G --keylen.min=1 --keylen.max=250 --datalen.min=1 --datalen.max=500 \
-				--nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) \
-				--hill | bzip2 -c > ${TESTDB_PREFIX}.log.bz2
-			./mdbx_chk -nvv ${TESTDB_PREFIX} | tee ${TESTDB_PREFIX}-chk.log
+function probe {
+	echo "=============================================== $(date)"
+	echo "${caption}: $*"
+	rm -f ${TESTDB_PREFIX}* \
+		&& ./mdbx_test --pathname=${TESTDB_PREFIX}db "$@" | lz4 > ${TESTDB_PREFIX}log.lz4 \
+		&& ./mdbx_chk -nvv ${TESTDB_PREFIX}db | tee ${TESTDB_PREFIX}chk \
+		|| (echo "FAILED"; exit 1)
+}
+
+###############################################################################
+
+count=0
+for nops in {2..7}; do
+	for ((wbatch=nops-1; wbatch > 0; --wbatch)); do
+		loops=$(((3333 >> nops) / nops + 1))
+		for ((rep=0; rep++ < loops; )); do
+			for ((bits=2**${#options[@]}; --bits >= 0; )); do
+				seed=$(date +%N)
+				caption="Probe #$((++count)) int-key,w/o-dups, repeat ${rep} of ${loops}" probe \
+					--pagesize=min --size=6G --table=+key.integer,-data.dups --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=1111 \
+					--nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) \
+					--keygen.seed=${seed} basic
+				caption="Probe #$((++count)) int-key,with-dups, repeat ${rep} of ${loops}" probe \
+					--pagesize=min --size=6G --table=+key.integer,+data.dups --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max \
+					--nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) \
+					--keygen.seed=${seed} basic
+				caption="Probe #$((++count)) int-key,int-data, repeat ${rep} of ${loops}" probe \
+					--pagesize=min --size=6G --table=+key.integer,+data.integer --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max \
+					--nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) \
+					--keygen.seed=${seed} basic
+				caption="Probe #$((++count)) w/o-dups, repeat ${rep} of ${loops}" probe \
+					--pagesize=min --size=6G --table=-data.dups --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=1111 \
+					--nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) \
+					--keygen.seed=${seed} --hill
+				caption="Probe #$((++count)) with-dups, repeat ${rep} of ${loops}" probe \
+					--pagesize=min --size=6G --table=+data.dups --keylen.min=min --keylen.max=max --datalen.min=min --datalen.max=max \
+					--nops=$( rep9 $nops ) --batch.write=$( rep9 $wbatch ) --mode=$(bits2list options $bits) \
+					--keygen.seed=${seed} --hill
+			done
 		done
 	done
 done
