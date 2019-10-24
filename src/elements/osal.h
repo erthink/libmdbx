362c00107012931eaa4e5e19eb2550b522c96927
cryptozoidberg <crypto.zoidberg@gmail.com>
2019-10-24 14:19:14 +0200

    Get rid of macro redefinition warnings under VC

--- src/elements/osal.h
+++ src/elements/osal.h
@@ -118,7 +118,9 @@
 #endif
 
 #if defined(_WIN32) || defined(_WIN64)
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <tlhelp32.h>
 #include <windows.h>
 #include <winnt.h>
