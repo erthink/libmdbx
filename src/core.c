5f09ec73c99845122fe95e221e240f72ed8878ec
Leonid Yuriev <leo@yuriev.ru>
2020-11-07 16:23:50 +0300

    mdbx: return `MDBX_ENODATA` instead of `MDBX_EINVAL` for non-positioned/EOF cursor and `MDBX_GET_CURRENT`.
    Change-Id: I2adf76f8f662e77e5a6aa077344fab6b430975c4

--- src/core.c
+++ src/core.c
@@ -12384,7 +12384,7 @@ int mdbx_cursor_get(MDBX_cursor *mc, MDBX_val *key, MDBX_val *data,
   switch (op) {
   case MDBX_GET_CURRENT: {
     if (unlikely(!(mc->mc_flags & C_INITIALIZED)))
-      return MDBX_EINVAL;
+      return MDBX_ENODATA;
     MDBX_page *mp = mc->mc_pg[mc->mc_top];
     const unsigned nkeys = page_numkeys(mp);
     if (mc->mc_ki[mc->mc_top] >= nkeys) {
