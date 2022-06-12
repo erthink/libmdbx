9285db6ec206d9724b38981780cb1c0838b88b90
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2022-06-12 20:09:27 +0300

    mdbx-test: add workaround for modern libstd++ with CLANG < 4.x

--- test/base.h
+++ test/base.h
@@ -18,6 +18,13 @@
 #define NOMINMAX
 #endif
 
+/* Workaround for modern libstdc++ with CLANG < 4.x */
+#if defined(__SIZEOF_INT128__) && !defined(__GLIBCXX_TYPE_INT_N_0) &&          \
+    defined(__clang__) && __clang_major__ < 4
+#define __GLIBCXX_BITSIZE_INT_N_0 128
+#define __GLIBCXX_TYPE_INT_N_0 __int128
+#endif /* Workaround for modern libstdc++ with CLANG < 4.x */
+
 #if defined(_WIN32) || defined(_WIN64) || defined(_WINDOWS)
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0601 /* Windows 7 */
