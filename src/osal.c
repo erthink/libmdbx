aa9d2387e59da96d527b7f4dc47f068ec6740964
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2024-02-28 15:49:05 +0300

    mdbx: исправление `me_dxb_mmap.current > me_dxb_mmap.limit` и срабатывания соответствующей assert-проверки.
    Устранение упущения приводящего к нелогичной ситуации `me_dxb_mmap.curren > me_dxb_mmap.limit` при "дребезге" размера БД.
    В текущем понимании, последствий кроме срабатывания assert-проверки нет, а вероятность проявления близка к нулю.

--- src/osal.c
+++ src/osal.c
@@ -2581,7 +2581,7 @@ retry_mapview:;
           ptr_disp(map->base, size),
           ((map->current < map->limit) ? map->current : map->limit) - size);
     }
-    map->current = size;
+    map->current = (size < map->limit) ? size : map->limit;
   }
 
   if (limit == map->limit)
@@ -2742,6 +2742,7 @@ retry_mapview:;
     map->base = ptr;
   }
   map->limit = limit;
+  map->current = size;
 
 #if MDBX_ENABLE_MADVISE
 #ifdef MADV_DONTFORK
