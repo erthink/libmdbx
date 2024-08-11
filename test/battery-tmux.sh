7511f480a4f9ebc3d9f6a7c16f6e2ce66afe921c
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2024-08-11 09:42:32 +0300

    mdbx-testing: уменьшение мощности "tmux-батарейного" теста вдвое (под "более типовую" виртуалку).

--- test/battery-tmux.sh
+++ test/battery-tmux.sh
@@ -14,7 +14,7 @@ tmux -f ./test/tmux.conf new-session -d -s mdbx htop
 W=0
 for ps in min 4k max; do
 	for from in 1 30000; do
-		for n in 0 1 2 3 4 5 6 7; do
+		for n in 0 1 2 3; do
 			CMD="${TEST} --delay $((n * 7)) --page-size ${ps} --from ${from} --dir ${PREFIX}page-${ps}.from-${from}.${n}"
 			if [ $n -eq 0 ]; then
 				tmux new-window -t mdbx:$((++W)) -n "page-${ps}.from-${from}" -k -d "$CMD"
@@ -23,7 +23,7 @@ for ps in min 4k max; do
 				tmux split-window -t mdbx:$W -l 20% -d $CMD
 			fi
 		done
-		for n in 0 1 2 3 4 5 6 7; do
+		for n in 0 1 2 3; do
 			CMD="${TEST} --delay $((3 + n * 7)) --extra --page-size ${ps} --from ${from} --dir ${PREFIX}page-${ps}.from-${from}.${n}-extra"
 			if [ $n -eq 0 ]; then
 				tmux new-window -t mdbx:$((++W)) -n "page-${ps}.from-${from}-extra" -k -d "$CMD"
