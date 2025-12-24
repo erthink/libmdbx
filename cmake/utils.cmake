bf84d40b7b0557ba623d56d284f643579fdd5335
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2025-12-25 00:23:22 +0300

    mdbx-cmake: hotfix of build error due two version info sources.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -379,7 +379,7 @@ macro(semver_provide name source_root_directory build_directory_for_json_output 
       endif()
       if(_source_root STREQUAL _git_root AND EXISTS "${_git_root}/VERSION.json")
         message(
-          FATAL_ERROR
+          WARNING
             "Несколько источников информации о версии, допустим только один из: репозиторий git, либо файл VERSION.json"
         )
       endif()
